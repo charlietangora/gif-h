b952b7b4cb63082f17f6258701749471c5b0cc35
ginsweater <ctangora@gmail.com>
2013-12-07 01:15:52 -0600

    Edit description in header

--- gif.h
+++ gif.h
@@ -4,10 +4,16 @@
 // Public domain.
 //
 // This file offers a simple, very limited way to create animated GIFs directly in code.
-// There's not much effort to optimize colors to improve compression, so resulting files
-// are often quite large. But it's still handy to be able to spit out GIFs on command.
 //
-// Only RGBA8 is currently supported as an input format; the alpha is ignored.
+// Those looking for particular cleverness are likely to be disappointed; it's pretty 
+// much a straight-ahead implementation of the GIF format with optional Floyd-Steinberg 
+// dithering. (It does at least use delta encoding - only the changed portions of each 
+// frame are saved.) 
+//
+// So resulting files are often quite large. The hope is that it will be handy nonetheless
+// as a quick and easily-integrated way for programs to spit out animations.
+//
+// Only RGBA8 is currently supported as an input format. (The alpha is ignored.)
 //
 // USAGE:
 // Create a GifWriter struct. Pass it to GifBegin() to initialize and write the header.
