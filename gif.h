667ba6c25dc0d7c851c09b64cc0587e10f62956f
Jacob Young <jacobly0@users.noreply.github.com>
2016-01-18 15:29:32 -0500

    Removed fopen so the user can use their preferred variant.

--- gif.h
+++ gif.h
@@ -731,15 +731,10 @@ struct GifWriter
 // Creates a gif file.
 // The input GIFWriter is assumed to be uninitialized.
 // The delay value is the time between frames in hundredths of a second - note that not all viewers pay much attention to this value.
-bool GifBegin( GifWriter* writer, const char* filename, uint32_t width, uint32_t height, uint32_t delay, int32_t bitDepth = 8, bool dither = false )
+bool GifBegin( GifWriter* writer, FILE *file, uint32_t width, uint32_t height, uint32_t delay, int32_t bitDepth = 8, bool dither = false )
 {
-#if _MSC_VER >= 1400
-	writer->f = 0;
-    fopen_s(&writer->f, filename, "wb");
-#else
-    writer->f = fopen(filename, "wb");
-#endif
-    if(!writer->f) return false;
+    if(!file) return false;
+    writer->f = file;
 
     writer->firstFrame = true;
 
